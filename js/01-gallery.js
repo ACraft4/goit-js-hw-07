451b4f100b703a6dc09324008f96c37739b02556
Artur Kravchenko <arturkra92@gmail.com>
2022-01-06 19:41:44 +0200

    Fix problems with path

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const arrayGalleryItems = galleryItems;
+
+const createContainer = arrayGalleryItems.map((items) => `<div class="gallery__item">
+  <a class="gallery__link"  href="#">
+    <img
+      class="gallery__image"
+      src="${items.original}"
+      data-source="large-image.jpg"
+      alt="items description"
+    />
+  </a>
+</div>
+`);
+
+galleryContainer.insertAdjacentHTML('beforeend', createContainer.join(''));
+
+galleryContainer.addEventListener('click', onOriginalImage)
+
+function onOriginalImage(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.src}" width="800" height="600">`);
+    instance.show()
+    window.addEventListener('keydown', (even) => {
+      if (even.key === 'Escape') {
+          instance.close()
+      }
+  })
+};
